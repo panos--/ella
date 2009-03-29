54b23515c0c5364451636f71eee2a8b2cca4b5d2
Thomas Weiss <panos@unbunt.org>
2009-03-29 14:44:14 +0200

    Made primitive expression objects in class Int static final for a slight performance improvement

--- src/org/unbunt/sqlscript/lang/Int.java
+++ src/org/unbunt/sqlscript/lang/Int.java
@@ -5,16 +5,24 @@ import org.unbunt.sqlscript.statement.*;
 public class Int extends BaseObj {
     protected static Int instance = null;
 
+    public static final PrimIntAddExpression primIntAdd = new PrimIntAddExpression();
+    public static final PrimIntSubExpression primIntSub = new PrimIntSubExpression();
+    public static final PrimIntMulExpression primIntMul = new PrimIntMulExpression();
+    public static final PrimIntDivExpression primIntDiv = new PrimIntDivExpression();
+    public static final PrimIntModExpression primIntMod = new PrimIntModExpression();
+    public static final PrimIntEqExpression primIntEq = new PrimIntEqExpression();
+    public static final PrimIntNeExpression primIntNe = new PrimIntNeExpression();
+
     private Int() {
-        primitives.put(new Str("+").intern(), new PrimIntAddExpression());
-        primitives.put(new Str("-").intern(), new PrimIntSubExpression());
-        primitives.put(new Str("*").intern(), new PrimIntMulExpression());
-        primitives.put(new Str("/").intern(), new PrimIntDivExpression());
-        primitives.put(new Str("%").intern(), new PrimIntModExpression());
-        primitives.put(new Str("==").intern(), new PrimIntEqExpression());
-        primitives.put(new Str("!=").intern(), new PrimIntNeExpression());
-        primitives.put(new Str("===").intern(), new PrimIntEqExpression());
-        primitives.put(new Str("!==").intern(), new PrimIntNeExpression());
+        primitives.put(new Str("+").intern(), primIntAdd);
+        primitives.put(new Str("-").intern(), primIntSub);
+        primitives.put(new Str("*").intern(), primIntMul);
+        primitives.put(new Str("/").intern(), primIntDiv);
+        primitives.put(new Str("%").intern(), primIntMod);
+        primitives.put(new Str("==").intern(), primIntEq);
+        primitives.put(new Str("!=").intern(), primIntNe);
+        primitives.put(new Str("===").intern(), primIntEq);
+        primitives.put(new Str("!==").intern(), primIntNe);
     }
 
     public static synchronized Int getInstance() {
