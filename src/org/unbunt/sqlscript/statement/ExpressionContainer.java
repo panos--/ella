3805ed9fb11b6ea9ccdf43d1f5b23e1262f88119
Thomas Weiss <panos@unbunt.org>
2009-07-16 23:32:20 +0200

    Removed some more stuff.

--- src/org/unbunt/sqlscript/statement/ExpressionContainer.java
+++
@@ -1,5 +0,0 @@
-package org.unbunt.sqlscript.statement;
-
-public interface ExpressionContainer extends Statement {
-    public void addStatement(Expression expression);
-}
--- src/org/unbunt/sqlscript/statement/StatementContainer.java
+++ src/org/unbunt/sqlscript/statement/StatementContainer.java
@@ -1,5 +1,6 @@
 package org.unbunt.sqlscript.statement;
 
-public interface StatementContainer extends ExpressionContainer {
+public interface StatementContainer extends Expression {
     public void addStatement(Statement statement);
+    public void addStatement(Expression expression);
 }
